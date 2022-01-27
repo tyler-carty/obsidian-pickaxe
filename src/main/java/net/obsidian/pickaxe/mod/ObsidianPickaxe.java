62f08162ca5ec129b1aeacb5ca61cc58a8fd074f
Tyler Cartwright <tcartwright2606@gmail.com>
2022-01-27 16:48:44 +0000

    Pickaxe Mod Initial Template

---
+++ src/main/java/net/obsidian/pickaxe/mod/ObsidianPickaxe.java
@@ -0,0 +1,13 @@
+package net.obsidian.pickaxe.mod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ObsidianPickaxe implements ModInitializer{
+
+    @Override
+    public void onInitialize() {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
